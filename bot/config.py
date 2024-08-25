145d671c78838939c74aac65dbd705883973dd4b
Hetlr88 <mrhetlr0@gmail.com>
2024-08-25 20:21:51 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -20,7 +20,7 @@ class Config(object):
         int(x) for x in get_config(
             "AUTH_USERS",
             should_prompt=True
-        ).split(6169288210)
+        ).split()
     )
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
